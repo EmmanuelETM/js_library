fda43fe22e4d30ad6515f785d5b08cb43b8f16a2
Emmanuel Torres Malena <torresmalenaemmanuel@gmail.com>
2025-01-06 23:02:25 -0400

    Working on styles and html. Researched various colors from tailwind css

--- app.js
+++ app.js
@@ -21,7 +21,6 @@ function addBooks(title, author, pages, read, library) {
 
 function displayBooks(library) { 
     library.forEach(book => {
-        console.log(book);
         let card = document.createElement('div');
         card.innerText = book.title;
         card.classList.add('card');
@@ -29,11 +28,12 @@ function displayBooks(library) {
     });
 }
 
+
 const container = document.querySelector('.cards-container');
 const book1 = new Book('The Hobbit', 'J.R.R Tolkien', 295, false);
 const book2 = new Book('Atomic Habits', 'James Clear ', 306, true);
 const book3 = new Book('Book3', 'MyBook', 420, false);
 
-const Library = [book1, book2, book3];
+const Library = [book1, book2, book2, book3, book3, book2, ];
 
 displayBooks(Library);
