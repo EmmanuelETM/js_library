7b2e86c8a99445c993ef7eb043fbb89c30edb6e4
Emmanuel Torres Malena <torresmalenaemmanuel@gmail.com>
2025-01-08 14:09:23 -0400

    Added the functionality to create books and display them, working now on the styles of the card and header

--- app.js
+++ app.js
@@ -14,12 +14,9 @@ class Book {
     }
 }
 
-function addBooks(title, author, pages, read, library) {
-    let newBook = new Book(title, author, pages, read);
-    library.push(newBook);
-} 
-
 function displayBooks(library) { 
+    container.innerHTML = '';
+    
     library.forEach(book => {
         let card = document.createElement('div');
         card.innerText = book.title;
@@ -28,32 +25,48 @@ function displayBooks(library) {
     });
 }
 
+function addBooks(obj, library) {
+    let newBook = null
+    if('read' in obj){
+        newBook = new Book(obj.title, obj.author, obj.pages, true);
+    } else {
+        newBook = new Book(obj.title, obj.author, obj.pages, false);
+    }
+    library.push(newBook);
+    console.log(library);
+} 
+
 function handleFormSubmit(event) {
     event.preventDefault();
     let data = new FormData(event.target);
-    const obj = Object.fromEntries(data.entries());
+    dataObj = Object.fromEntries(data.entries());
+    addBooks(dataObj, Library);
+    displayBooks(Library);
     form.reset();
     dialog.close();
 }
 
 const container = document.querySelector('.cards-container');
 const addBtn = document.querySelector('.add-book');
+const closeBtn = document.querySelector('.close-button');
 const dialog = document.querySelector('#dialog');
 const form = document.querySelector('#form');
 
+let dataObj = null;
 const book1 = new Book('The Hobbit', 'J.R.R Tolkien', 295, false);
 const book2 = new Book('Atomic Habits', 'James Clear ', 306, true);
 const book3 = new Book('Book3', 'MyBook', 420, false);
-const Library = [book1, book2, book2, book3, book3, book2];
+const Library = [book1, book2];
 
 
-
-addBtn.addEventListener('click', (event) => {
+addBtn.addEventListener('click', () => {
     dialog.showModal();
-})
+});
+
+closeBtn.addEventListener('click', () => {
+    dialog.close();
+});
 
 form.addEventListener('submit', (event) => handleFormSubmit(event));
 
-
-
-// displayBooks(Library);
+displayBooks(Library);
