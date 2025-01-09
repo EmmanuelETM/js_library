583bcdd8a20e518e7baa644d44565fdc48bf217c
Emmanuel Torres Malena <torresmalenaemmanuel@gmail.com>
2025-01-09 09:23:32 -0400

    Finished with the App logic, working now on the card styles

--- app.js
+++ app.js
@@ -16,14 +16,73 @@ class Book {
 
 function displayBooks(library) { 
     container.innerHTML = '';
-    
-    library.forEach(book => {
-        let card = document.createElement('div');
-        card.innerText = book.title;
-        card.classList.add('card');
-        container.appendChild(card);
+
+    library.forEach((book, index) => {
+        createCard(book, index);
     });
 }
+
+function deleteBook (event) {
+    let card = event.target.parentElement;
+    const BookIndex = Number(card.getAttribute('data-index'));
+    Library.splice(BookIndex, 1);
+    displayBooks(Library);
+}   
+
+function handleCardCheckbox (event) {
+    let card = event.target.parentElement;
+    const BookIndex = Number(card.getAttribute('data-index'));
+
+    if(event.target.checked) {
+        Library[BookIndex].setRead = true;
+    }
+    else {
+        Library[BookIndex].setRead = false;
+    }
+
+    console.log(Library[BookIndex]);
+}
+
+function createCard (book, index) {
+    let newBook = null;
+    let card = document.createElement('div');
+    let title = document.createElement('h3');
+    let author = document.createElement('p');
+    let pages = document.createElement('p');
+    let label = document.createElement('label');
+    let read = document.createElement('input');
+    let deleteBtn = document.createElement('button');
+
+    card.classList.add('card');
+    card.setAttribute('data-index', `${index}`);
+
+    title.innerText = `${book.title}`;
+    author.innerText = `Author: ${book.author}`;
+    pages.innerText = `Pages: ${book.pages}`;
+    label.innerText = `Have read`;
+    label.setAttribute('for', `card-checkbox${index}`);
+    read.setAttribute('type', 'checkbox');
+    read.setAttribute('id', `card-checkbox${index}`);
+    read.setAttribute('name', `card-checkbox${index}`);
+
+    if(book.read) read.checked = true;
+
+    read.addEventListener('change', handleCardCheckbox);
+
+    deleteBtn.innerText = 'Delete';
+    deleteBtn.addEventListener('click', deleteBook);
+
+    card.appendChild(title);
+    card.appendChild(author);
+    card.appendChild(pages);
+    card.appendChild(label);
+    card.appendChild(read);
+    card.appendChild(deleteBtn);
+    container.appendChild(card);
+    return newBook;
+}
+
+
 
 function addBooks(obj, library) {
     let newBook = null
@@ -33,8 +92,9 @@ function addBooks(obj, library) {
         newBook = new Book(obj.title, obj.author, obj.pages, false);
     }
     library.push(newBook);
-    console.log(library);
 } 
+
+
 
 function handleFormSubmit(event) {
     event.preventDefault();
@@ -44,6 +104,19 @@ function handleFormSubmit(event) {
     displayBooks(Library);
     form.reset();
     dialog.close();
+}
+
+const MyApp = () => {
+    addBtn.addEventListener('click', () => {
+        dialog.showModal();
+    });
+
+    closeBtn.addEventListener('click', () => {
+        dialog.close();
+    });
+
+    form.addEventListener('submit', (event) => handleFormSubmit(event));
+    displayBooks(Library);
 }
 
 const container = document.querySelector('.cards-container');
@@ -56,17 +129,7 @@ let dataObj = null;
 const book1 = new Book('The Hobbit', 'J.R.R Tolkien', 295, false);
 const book2 = new Book('Atomic Habits', 'James Clear ', 306, true);
 const book3 = new Book('Book3', 'MyBook', 420, false);
-const Library = [book1, book2];
+const Library = [book1, book2, book1, book2, book1, book2, book1, book2, book1, book2, book1, book2];
 
+MyApp();
 
-addBtn.addEventListener('click', () => {
-    dialog.showModal();
-});
-
-closeBtn.addEventListener('click', () => {
-    dialog.close();
-});
-
-form.addEventListener('submit', (event) => handleFormSubmit(event));
-
-displayBooks(Library);
